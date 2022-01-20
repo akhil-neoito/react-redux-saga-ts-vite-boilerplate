49f7bc25119daa99725d9da6c303e25e160e5e96
akhil-chandran <akhil.chandran@neoito.com>
2022-01-20 21:10:42 +0530

    fix: style fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps
 }
 
 const defaultClasses =
-  'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800';
+  'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 min-w-[100px]';
 
 const Button: React.FC<ButtonProps> = ({
   text,
