4a2fe76709403a0187707fadb571f426387ec400
akhil-chandran <akhil.chandran@neoito.com>
2022-01-20 21:36:23 +0530

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './App.css'
 import Button from './components/Button';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="App font-mono">
+      <Header />
       <Button text="Increment" onClick={() => setCount(count + 1)} />
     </div>
   );
