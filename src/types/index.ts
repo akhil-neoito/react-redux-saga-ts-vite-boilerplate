1965b516fbd7b6b64f24b1c664db0ea902cfe54e
akhil-chandran <akhil.chandran@neoito.com>
2022-03-04 11:22:30 +0530

    refactor: add proper comments to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,32 @@
+/**
+ * Generic type to create a type that can be null or undefined.
+ *
+ * @param T The type.
+ *
+ * @example
+ * type MaybeString = Maybe<string>; // string | null | undefined
+ *
+ */
 export type Maybe<T> = T | null | undefined;
+
+/**
+ * Generic type to create a type that can be nullified.
+ *
+ * @param T The type.
+ *
+ * @example
+ * type NullableString = Nullable<string>; // string | null
+ *
+ */
 export type Nullable<T> = T | null;
 
 /**
  * Used to override default function signatures
  *
- * @example:
+ * @param BaseFunction - The function to override
+ * @param T - Custom arguments
+ *
+ * @example
  * const calculateAreaOfSquare: (a: number) => number = (a: number) => a * a;
  * const calculateAreaofRectangle: DerivedFunction<typeof calculateAreaOfSquare, [number]> = (a: number, b: number) => a * b;
  * typeof calculateAreaofRectangle // (a: number, b: number) => number
@@ -16,6 +38,26 @@ export type DerivedFunction<
   T extends unknown[] = unknown[]
 > = BaseFunction extends (...a: infer U) => infer R
   ? (...a: [...U, ...T]) => R
+  : never;
+
+/**
+ * Converts string or template literals to UPPER_SNAKE_CASE
+ *
+ * @param S - String or template literal
+ * @param D - Delimiter
+ *
+ * @example
+ * type NewType = UpperSnakecase<'hello world'>; // HELLO_WORLD
+ * type NewType = UpperSnakecase<'hello:world' | 'test:arg', ':'>; // HELLO_WORLD | TEST_ARG
+ *
+ */
+export type UpperSnakecase<
+  S extends string,
+  D extends string = ''
+> = S extends `${infer FirstWord}${D}${infer Rest}`
+  ? D extends ''
+    ? Uppercase<S>
+    : `${Uppercase<FirstWord>}_${Uppercase<Rest>}`
   : never;
 
 // Generic type for generator function that returns void
